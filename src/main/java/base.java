99a37f2ae8188ada4834f6d9d4e8499bd32d152d
mohammad rafi shaik <rafitest444@hotmail.com>
2024-01-15 10:36:04 +0300

    test

--- src/main/java/base.java
+++ src/main/java/base.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -11,7 +12,14 @@ public class base {
     @BeforeTest
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+//        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); //!!!should be enabled for Jenkins
+        options.addArguments("--disable-dev-shm-usage"); //!!!should be enabled for Jenkins
+        options.addArguments("--window-size=1920x1080"); //!!!should be enabled for Jenkins
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
     }
 
     @AfterTest
